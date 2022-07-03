e50050ca0514fdab84f6718f537792696782a625
Pooja_K <kanadepooja789@gmail.com>
2022-07-03 13:37:43 +0530

    run progression bar with time updates

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let songIndex = 0;
 let audioElement = new Audio('./playlist/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
     {songName: "RAj aal", filePath: "playlist/1.mp3", coverPath: "covers/1.jpg"},
@@ -34,17 +35,22 @@ masterPlay.addEventListener('click', ()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
     }
     else {
         audioElement.pause();
         masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
     }
 
 })
 // Listen to events 
-myProgressBar.addEventListener('timeupdate', ()=>{
+audioElement.addEventListener('timeupdate', ()=>{
     console.log('timeupdate')
     // update seekbar
-    
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    console.log(progress)
+    myProgressBar.value = progress;
+
 })
